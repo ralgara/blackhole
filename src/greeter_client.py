13eb287e7c8aacb9ec8d3b02c167335c5c109001
Rafael Algara <rafael.algara@clarabridge.com>
2017-11-16 03:28:45 -0500

    Add official helloworld files

---
+++ src/greeter_client.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+import grpc
+
+import sys
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+
+def run():
+      channel = grpc.insecure_channel('localhost:50051')
+      stub = helloworld_pb2_grpc.GreeterStub(channel)
+      response = stub.SayHello(helloworld_pb2.HelloRequest(name=sys.argv[1]))
+      print("Greeter client received: " + response.message)
+
+
+if __name__ == '__main__':
+    run()
---
+++ src/greeter_server.py
@@ -0,0 +1,40 @@
+from concurrent import futures
+import time
+
+import grpc
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+
+    def SayHello(self, request, context):
+        msg = 'Hello, %s!' % request.name
+        print msg
+        return helloworld_pb2.HelloReply(message=msg)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+
+    print "------------------------------------"
+    print " Server started "
+    print "------------------------------------"
+
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    serve()
+    print "------------------------------------"
+    print " Server stopped"
+    print "------------------------------------"
