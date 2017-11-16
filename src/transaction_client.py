d4427815cc9f756da86e4c1f7364bc415e7528d2
Rafael Algara <rafael.algara@clarabridge.com>
2017-11-16 03:32:14 -0500

    Add grpc stubs to client

--- src/transaction_client.py
+++ src/transaction_client.py
@@ -1,10 +1,19 @@
-import transaction_pb2 as txn
+import grpc
 import pdb
+import transaction_pb2 as txn
+import transaction_pb2_grpc as txn_grpc
 
 request = txn.BankRequest()
 request.name = 'Satoshi'
 request.account = '0001200-ABF-391'
 request.type = txn.CREDIT
 request.amount = 3.14
+
 #print request.SerializeToString()
 #pdb.set_trace()
+
+channel = grpc.insecure_channel('localhost:50051')
+stub = txn_grpc.BankStub(channel)
+reply = stub.Deposit(request)
+
+print "Transaction completed: {message}, balance: {balance}".format(**reply)
